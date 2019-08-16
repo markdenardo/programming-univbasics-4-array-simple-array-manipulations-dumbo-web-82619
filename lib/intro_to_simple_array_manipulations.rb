ecf118a10a6e585a637d21eee3606eb2fb6b4c34
Mark DeNardo <mark.denardo@gmail.com>
2019-08-16 00:25:07 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,10 +1,10 @@
-def using_push(colors_in_the_rainbow,next_color)
+def using_push(array, string)
 colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
 next_color = "violet"
 colors_in_the_rainbow.push("violet")
 end
 
-def using_unshift(bouroughs_in_nyc,new_neighborhood)
+def using_unshift(array,string)
   bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
   new_neighborhood = "Staten Island"
   bouroughs_in_nyc.unshift("Staten Island")
