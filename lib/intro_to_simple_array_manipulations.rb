683b263264310820b50493bf79772b89b54ab0bc
Mark DeNardo <mark.denardo@gmail.com>
2019-08-16 00:35:15 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -25,6 +25,7 @@ def using_shift(array)
 end
 
 def shift_with_args(array)
+  array.shift(2)
   
   #This method takes in an argument of an Array and uses the .shift method with an argument of 2 to remove and return the first 2 items from the array.
 end
